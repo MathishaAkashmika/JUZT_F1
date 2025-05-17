211dc432d57ed5fb75d01207b6257c492306725b
Prabudda Perera <mathishaflame9@gmail.com>
2025-05-17 22:33:45 +0530

    removed backend

--- backend/src/laps/dto/update-lap.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateLapDto } from './create-lap.dto';
-
-export class UpdateLapDto extends PartialType(CreateLapDto) {}
--- backend/src/laps/laps.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LapsService } from './laps.service';
-import { LapsController } from './laps.controller';
-
-@Module({
-  controllers: [LapsController],
-  providers: [LapsService],
-})
-export class LapsModule {}
